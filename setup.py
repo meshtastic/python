67ae7aca3f0e98b56b4a6b3bbf0dcb7df44387a5
geeksville <kevinh@geeksville.com>
2020-06-28 11:48:49 -0700

    0.7.8 add pygatt dependency, fix bug noticed by @slavino in #3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 # This call to setup() does all the work
 setup(
     name="meshtastic",
-    version="0.7.7",
+    version="0.7.8",
     description="Python API & client shell for talking to Meshtastic devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -26,7 +26,8 @@ setup(
     packages=["meshtastic"],
     include_package_data=True,
     install_requires=["pyserial>=3.4", "protobuf>=3.6.1",
-                      "pypubsub>=4.0.3", "dotmap>=1.3.14", "pexpect"],
+                      "pypubsub>=4.0.3", "dotmap>=1.3.14", "pexpect>=4.6.0",
+                      "pygatt>=4.0.5"],
     python_requires='>=3.4',
     entry_points={
         "console_scripts": [
