33b31d0fb2773b0f9c7b04ec7f92ae9a4255f7b6
geeksville <kevinh@geeksville.com>
2020-09-11 13:06:21 -0700

    1.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 # This call to setup() does all the work
 setup(
     name="meshtastic",
-    version="1.0.2",
+    version="1.0.3",
     description="Python API & client shell for talking to Meshtastic devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
