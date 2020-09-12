23f4a50f911578eb58dc45f5045e838a7805ede4
geeksville <kevinh@geeksville.com>
2020-09-12 12:30:19 -0700

    OS-X was rebooting devices on connect, force RTS false (RTS is connected to ESP32 reset).  Thanks to @mc-hamster for this fix!

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 # This call to setup() does all the work
 setup(
     name="meshtastic",
-    version="1.0.3",
+    version="1.0.4",
     description="Python API & client shell for talking to Meshtastic devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
