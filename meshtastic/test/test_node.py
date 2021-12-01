4def6438033dc3408507d6aad238170f071b03aa
Mike Kinney <mike.kinney@gmail.com>
2021-12-01 00:24:53 -0800

    break out unit tests so there is one assert per test

--- meshtastic/test/test_node.py
+++ meshtastic/test/test_node.py
@@ -8,9 +8,30 @@ import pytest
 from meshtastic.node import pskToString
 
 @pytest.mark.unit
-def test_pskToString():
-    """Test pskToString"""
+def test_pskToString_empty_string():
+    """Test pskToString empty string"""
     assert pskToString('') == 'unencrypted'
+
+
+@pytest.mark.unit
+def test_pskToString_string():
+    """Test pskToString string"""
+    assert pskToString('hunter123') == 'secret'
+
+
+@pytest.mark.unit
+def test_pskToString_one_byte_zero_value():
+    """Test pskToString one byte that is value of 0"""
     assert pskToString(bytes([0x00])) == 'unencrypted'
+
+
+@pytest.mark.unit
+def test_pskToString_one_byte_non_zero_value():
+    """Test pskToString one byte that is non-zero"""
     assert pskToString(bytes([0x01])) == 'default'
+
+
+@pytest.mark.unit
+def test_pskToString_many_bytes():
+    """Test pskToString many bytes"""
     assert pskToString(bytes([0x02, 0x01])) == 'secret'
