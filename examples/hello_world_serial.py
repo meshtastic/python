0701d400ffa5a805c86989d3dd01c3148093058a
Mike Kinney <mike.kinney@gmail.com>
2021-12-21 11:21:37 -0800

    add examples; add Makefile

---
+++ examples/hello_world_serial.py
@@ -0,0 +1,18 @@
+"""Simple program to demo how to use meshtastic library.
+   To run: python examples/hello_world_serial.py
+"""
+
+import sys
+import meshtastic
+import meshtastic.serial_interface
+
+# simple arg check
+if len(sys.argv) < 2:
+    print(f"usage: {sys.argv[0]} message")
+    sys.exit(3)
+
+# By default will try to find a meshtastic device,
+# otherwise provide a device path like /dev/ttyUSB0
+iface = meshtastic.serial_interface.SerialInterface()
+iface.sendText(sys.argv[1])
+iface.close()
