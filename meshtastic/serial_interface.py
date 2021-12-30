181c04716a89e453bf1406f4777b3a3a60fbe40a
Mike Kinney <mike.kinney@gmail.com>
2021-12-29 20:55:31 -0800

    accidentally dropped an arg

--- meshtastic/serial_interface.py
+++ meshtastic/serial_interface.py
@@ -49,7 +49,7 @@ class SerialInterface(StreamInterface):
                     f.close()
                 time.sleep(0.1)
 
-        self.stream = serial.Serial(devPath, 921600, exclusive=True, timeout=0.5)
+        self.stream = serial.Serial(devPath, 921600, exclusive=True, timeout=0.5, write_timeout=0)
         if not self.noProto:
             self.stream.flush()
             time.sleep(0.1)
