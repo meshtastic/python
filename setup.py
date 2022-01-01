9380f048fab3c05bed2d262bbff5102bd25b6a63
mkinney <mike.kinney@gmail.com>
2022-01-01 11:11:54 -0800

    Update setup.py
    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 # This call to setup() does all the work
 setup(
     name="meshtastic",
-    version="1.2.49",
+    version="1.2.50",
     description="Python API & client shell for talking to Meshtastic devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
