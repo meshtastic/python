fe69f05e75d4ab7b001b37171bce26844aeb79e7
Mike Kinney <mike.kinney@gmail.com>
2022-01-05 11:20:04 -0800

    add python 3.6, 3.7, 3.8, and 3.9 for ci and validation

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 # This call to setup() does all the work
 setup(
     name="meshtastic",
-    version="1.2.51",
+    version="1.2.52",
     description="Python API & client shell for talking to Meshtastic devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,7 +23,10 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=["meshtastic"],
     include_package_data=True,
