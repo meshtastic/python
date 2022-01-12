b6570e3c27fba13c4c913ae4ae53f9e6476599ad
Mike Kinney <mike.kinney@gmail.com>
2022-01-12 10:23:51 -0800

    simple example showing how you can set the long and short name

---
+++ examples/set_owner.py
@@ -0,0 +1,20 @@
+"""Simple program to demo how to use meshtastic library.
+   To run: python examples/set_owner.py Bobby 333
+"""
+
+import sys
+import meshtastic
+import meshtastic.serial_interface
+
+# simple arg check
+if len(sys.argv) < 2:
+    print(f"usage: {sys.argv[0]} long_name [short_name]")
+    sys.exit(3)
+
+iface = meshtastic.serial_interface.SerialInterface()
+long_name = sys.argv[1]
+short_name = None
+if len(sys.argv) > 2:
+    short_name = sys.argv[2]
+iface.localNode.setOwner(long_name, short_name)
+iface.close()
