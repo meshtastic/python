4f7f38e0a7873479127999ea85df892c8366f923
mkinney <mike.kinney@gmail.com>
2022-02-18 11:16:49 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 # This call to setup() does all the work
 setup(
     name="meshtastic",
-    version="1.2.84",
+    version="1.2.85",
     description="Python API & client shell for talking to Meshtastic devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
