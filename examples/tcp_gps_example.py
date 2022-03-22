3332271a97eddb3291ada315011165305ec46d57
Mike Schiraldi <git@schiraldi.org>
2022-03-22 12:41:50 -0700

    lint

--- examples/tcp_gps_example.py
+++ examples/tcp_gps_example.py
@@ -1,12 +1,14 @@
-import sys
+"""Demonstration of how to look up a radio's location via its LAN connection.
+   Before running, connect your machine to the same WiFi network as the radio.
+"""
+
 import meshtastic
 import meshtastic.tcp_interface
 
-node_ip = "192.168.42.1"
-iface = meshtastic.tcp_interface.TCPInterface(node_ip)
+radio_hostname = "meshtastic.local"  # Can also be an IP
+iface = meshtastic.tcp_interface.TCPInterface(radio_hostname)
 my_node_num = iface.myInfo.my_node_num
 pos = iface.nodesByNum[my_node_num]["position"]
 print (pos)
 
 iface.close()
-
