473401a9d4af3c0129aed60f6c33606dde681506
Nerdenator <a.mcruer@live.com>
2024-06-25 21:19:19 -0500

    ble_code_coverage_1: hanging on first test was due to an open client. Closing it solves issue. Problem with third test case is more complex; needs a mock of Bluetooth device.

--- meshtastic/tests/test_ble_interface.py
+++ meshtastic/tests/test_ble_interface.py
@@ -1,35 +1,41 @@
 """Meshtastic unit tests for ble_interface.py"""
-# import logging
+import logging
 
-# from meshtastic.ble_interface import BLEClient
+import pytest
 
-# def test_ble_client_no_addr_logs_message(caplog):
-#     """
-#     We want to see a debug message describing the error
-#     if we try to initialize a BLEClient with no address.
-#     """
-#     caplog.set_level(level=logging.DEBUG)
-#     BLEClient(address=None)
-#     assert "No address provided - only discover method will work." in caplog.text
+from meshtastic.ble_interface import BLEClient, BLEInterface
 
-# def test_ble_interface_no_addr_returns_only_basic_object():
-#     """
-#     We want BLEState to be the only property of the BLEInterface if
-#     it's initialized with an address of None.
-#     """
-#     test_interface = BLEInterface(address=None)
-#     test_interface_dict = test_interface.__dict__
-#     assert len(test_interface_dict) == 1
-#     assert 'state' in test_interface_dict
-#     assert isinstance(test_interface_dict['state'], BLEInterface.BLEState)
 
-# TODO: figure out why GitHub Actions just hangs on this test.
-# Does it need to have the BT library need mocking?
+def test_ble_client_no_addr_logs_message(caplog):
+    """
+    We want to see a debug message describing the error
+    if we try to initialize a BLEClient with no address.
+    """
+    caplog.set_level(level=logging.DEBUG)
+    test_ble_client = BLEClient(address=None)
+    test_ble_client.close()
+    assert "No address provided - only discover method will work." in caplog.text
+    
+
+def test_ble_interface_no_addr_returns_only_basic_object():
+    """
+    We want BLEState to be the only property of the BLEInterface if
+    it's initialized with an address of None.
+    """
+    test_interface = BLEInterface(address=None)
+    test_interface_dict = test_interface.__dict__
+    assert len(test_interface_dict) == 1
+    assert 'state' in test_interface_dict
+    assert isinstance(test_interface_dict['state'], BLEInterface.BLEState)
+
+# TODO: this method definitely requires a Bluetooth device be present and on
+# TODO: mock out whatever it needs
 # def test_ble_interface_bogus_addr_exits_process():
 #     """
 #     If we initialize BLEInterface with a BT address that doesn't
 #     exist, we should exit the process
 #     """
 #     with pytest.raises(SystemExit) as exc:
-#         BLEInterface(address="bogus")
+#         test_ble_int = BLEInterface(address="bogus")
 #     assert exc.value.code == 1
+#     test_ble_int.client.close()
