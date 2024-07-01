49bd9cb515fbbafce71860436783f2596dd241d9
Ian McEwen <mian@cyverse.org>
2024-06-30 23:40:11 -0700

    Support --ble on older firmwares that don't have the log-radio characteristic yet.

--- meshtastic/ble_interface.py
+++ meshtastic/ble_interface.py
@@ -56,7 +56,8 @@ class BLEInterface(MeshInterface):
             self.close()
             raise e
 
-        self.client.start_notify(LOGRADIO_UUID, self.log_radio_handler)
+        if self.client.has_characteristic(LOGRADIO_UUID):
+            self.client.start_notify(LOGRADIO_UUID, self.log_radio_handler)
 
         logging.debug("Mesh configure starting")
         self._startConfig()
@@ -247,6 +248,12 @@ class BLEClient:
 
     def write_gatt_char(self, *args, **kwargs):  # pylint: disable=C0116
         self.async_await(self.bleak_client.write_gatt_char(*args, **kwargs))
+
+    def has_characteristic(self, specifier):
+        if self.bleak_client.services.get_characteristic(specifier):
+            return True
+        else:
+            return False
 
     def start_notify(self, *args, **kwargs):  # pylint: disable=C0116
         self.async_await(self.bleak_client.start_notify(*args, **kwargs))
