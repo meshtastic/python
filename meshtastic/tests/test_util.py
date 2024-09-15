2026212a00665aea576937055b22309f8cb38c24
Derek Arnold <derek@derekarnold.net>
2024-09-15 16:27:40 -0500

    please pylint with a docstring and newline

--- meshtastic/tests/test_util.py
+++ meshtastic/tests/test_util.py
@@ -652,6 +652,7 @@ def test_fuzz_fromStr(valstr):
             assert isinstance(result, str)
 
 def test_shorthex():
+    """Test the shortest hex string representations"""
     result = fromStr('0x0')
     assert result == b'\x00'
     result = fromStr('0x5')
