f9ae021e4338787cf436a8a1a38cefa226290c18
Michael Gillett <51103663+migillett@users.noreply.github.com>
2025-02-18 14:23:40 -0500

    remove old implementation

--- meshtastic/mesh_interface.py
+++ meshtastic/mesh_interface.py
@@ -469,15 +469,6 @@ class MeshInterface:  # pylint: disable=R0902
         and can be used to track future message acks/naks.
         """
 
-        # issue 464: allow for 0x prefix in destinationId for hex values
-        # destination ids can either be integers or strings with a !prefix
-        try:
-            # sometimes the destinationId is actually an int as a string, so doing a type() check won't work
-            int(destinationId)
-        except ValueError:
-            # only take the last 8 characters of the hexadecimal destinationId and force a ! prefix
-            destinationId = f'!{destinationId[-8:]}'
-
         if getattr(data, "SerializeToString", None):
             logging.debug(f"Serializing protobuf as data: {stripnl(data)}")
             data = data.SerializeToString()
