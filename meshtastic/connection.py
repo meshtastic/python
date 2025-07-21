4b8ba4afa2f8b88b3feb46eb1040c36e2c3ff7df
flockofsparrows <flockofsparrows@riseup.net>
2025-07-21 12:45:39 -0400

    Refactor async connection init/teardown

--- meshtastic/connection.py
+++ meshtastic/connection.py
@@ -13,18 +13,23 @@ import serial_asyncio
 from meshtastic.protobuf.mesh_pb2 import FromRadio, ToRadio
 
 
-# magic number used in streaming client headers
-HEADER_MAGIC: bytes = b"\x94\xc3"
+STREAM_HEADER_MAGIC: bytes = b"\x94\xc3"  # magic number used in streaming client headers
+DEFAULT_BAUDRATE: int = 115200
 
 
-class ConnectionError(Exception):
+class MeshConnectionError(Exception):
     """Base class for MeshConnection-related errors."""
 
 
-class BadPayloadError(ConnectionError):
+class BadPayloadError(MeshConnectionError):
+    """Error indicating invalid payload over connection"""
     def __init__(self, payload, reason: str):
         self.payload = payload
         super().__init__(reason)
+
+
+class ConnectionTerminatedError(MeshConnectionError):
+    """Error indicating the connection was terminated."""
 
 
 class MeshConnection(ABC):
@@ -32,12 +37,10 @@ class MeshConnection(ABC):
 
     def __init__(self, name: str):
         self.name: str = name
+        self.on_ready: asyncio.Event = asyncio.Event()
         self.on_disconnect: asyncio.Event = asyncio.Event()
-        self._is_ready: bool = False
         self._send_lock: asyncio.Lock = asyncio.Lock()
         self._recv_lock: asyncio.Lock = asyncio.Lock()
-        self._init_task: asyncio.Task = asyncio.create_task(self._initialize())
-        self._init_task.add_done_callback(self._after_initialize)
 
     @abstractmethod
     async def _initialize(self):
@@ -45,14 +48,12 @@ class MeshConnection(ABC):
         (and therefore not from the constructor)."""
 
     @abstractmethod
-    async def _send_bytes(self, msg: buffer):
+    async def _send_bytes(self, msg: bytes):
         """Send bytes to the mesh device."""
-        pass
 
     @abstractmethod
-    async def _recv_bytes(self) -> buffer:
+    async def _recv_bytes(self) -> bytes:
         """Recieve bytes from the mesh device."""
-        pass
 
     @staticmethod
     @abstractmethod
@@ -61,44 +62,61 @@ class MeshConnection(ABC):
 
         Generates values that can be passed to the concrete connection class's
         constructor."""
-        pass
 
     def ready(self):
-        return self._is_ready
+        """Returns if the connection is ready for tx/rx"""
+        return self.on_ready.is_set()
 
-    def _after_initialize(self):
-        self._is_ready = True
-        del self._init_task
+    async def open(self):
+        """Start the connection"""
+        await self._initialize()
+        self.on_ready.set()
+
+    def _ensure_ready(self):
+        """Raise an exception if the connection is not ready for tx/rx"""
+        if not self.ready():
+            raise MeshConnectionError("Connection used before it was ready")
 
     async def send(self, message: ToRadio):
         """Send something to the connected device."""
+        self._ensure_ready()
         async with self._send_lock:
             msg_str: str = message.SerializeToString()
             await self._send_bytes(bytes(msg_str))
 
     async def recv(self) -> FromRadio:
         """Recieve something from the connected device."""
+        self._ensure_ready()
         async with self._recv_lock:
-            msg_bytes: buffer = await self._recv_bytes()
+            msg_bytes: bytes = await self._recv_bytes()
             return FromRadio.FromString(str(msg_bytes, errors="ignore"))
 
     async def listen(self) -> AsyncGenerator[FromRadio]:
+        """Yields new messages from the radio so long as the connection is active."""
+        self._ensure_ready()
         while not self.on_disconnect.is_set():
             yield await self.recv()
 
-    def close(self):
+    async def close(self):
         """Close the connection.
         Overloaders should remember to call supermethod"""
-        if not self.ready():
-            self._init_task.cancel()
-
+        self.on_ready.unset()
         self.on_disconnect.set()
 
-    def __enter__(self):
+    async def __aenter__(self):
+        await self.open()
         return self
 
-    def __exit__(self, exc_type, exc_value, trace):
-        self.close()
+    async def __aexit__(self, exc_type, exc_value, trace):
+        await self.close()
+
+    #def __enter__(self):
+    #    self.open()
+    #    asyncio.run(self._init_task)
+    #    return self
+
+    #def __exit__(self, exc_type, exc_value, trace):
+    #    self.close()
 
 
 class StreamConnection(MeshConnection):
@@ -113,12 +131,12 @@ class StreamConnection(MeshConnection):
         self.stream_debug_out.write(str(debug_out))
         self.stream_debug_out.flush()
 
-    async def _send_bytes(self, msg: buffer):
+    async def _send_bytes(self, msg: bytes):
         length: int = len(msg)
         if length > 512:
             raise BadPayloadError(msg, "Cannot send client API messages over 512 bytes")
 
-        self._writer.write(HEADER_MAGIC)
+        self._writer.write(STREAM_HEADER_MAGIC)
         self._writer.write(length.to_bytes(2, "big"))
         self._writer.write(msg)
         await self._writer.drain()
@@ -127,8 +145,8 @@ class StreamConnection(MeshConnection):
         """Consumes and logs debug out bytes until a valid header is detected"""
         try:
             while True:
-                from_stream: bytes = await self._reader.readuntil((b'\n', HEADER_MAGIC))
-                if from_stream.endswith(HEADER_MAGIC):
+                from_stream: bytes = await self._reader.readuntil((b'\n', STREAM_HEADER_MAGIC))
+                if from_stream.endswith(STREAM_HEADER_MAGIC):
                     self._handle_debug(from_stream[:-2])
                     return
                 else:
@@ -139,7 +157,7 @@ class StreamConnection(MeshConnection):
                 self._handle_debug(err.partial)
             raise
 
-    async def _recv_bytes(self) -> buffer:
+    async def _recv_bytes(self) -> bytes:
         try:
             while True:
                 await self._find_stream_header()
@@ -151,33 +169,34 @@ class StreamConnection(MeshConnection):
                 self._handle_debug(size_bytes)
 
         except asyncio.LimitOverrunError as err:
-            raise ConnectionError(
-                "Read buffer overrun while reading stream") from err
+            raise MeshConnectionError("Read buffer overrun while reading stream") from err
 
         except asyncio.IncompleteReadError:
             logging.error(f"Connection to {self.name} terminated: stream EOF reached")
-            self.close()
+            raise ConnectionTerminatedError from None
 
-    def close(self):
-        super().close()
+    async def close(self):
+        await super().close()
         self._writer.close()
         self.stream_debug_out.close()
-        asyncio.as_completed((self._writer.wait_closed(),))
+        await self._writer.wait_closed()
 
 
 class SerialConnection(StreamConnection):
-    def __init__(self, portaddr: str, baudrate: int=115200):
+    """Connection to a mesh radio over serial port"""
+    def __init__(self, portaddr: str, baudrate: int=DEFAULT_BAUDRATE):
         self.port: str = portaddr
         self.baudrate: int = baudrate
         super().__init__(portaddr)
 
     async def _initialize(self):
         self._reader, self._writer = await serial_asyncio.open_serial_connectio(
-            port=self._port, baudrate=self._baudrate,
-        )
+            port=self._port, baudrate=self._baudrate)
 
     @staticmethod
     async def get_available() -> AsyncGenerator[str]:
         for port in serial.tools.list_ports.comports():
+            # filtering for hwid gets rid of linux VT serials (e.g, /dev/ttyS0 and friends)
+            # FIXME: this may not be cross-platform or non-USB serial friendly
             if port.hwid != "n/a":
                 yield port.device
