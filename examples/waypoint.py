9285f7d13fc6c978b9192360c4eda8975de6fa32
pdxlocations <benlipsey@gmail.com>
2025-10-29 08:58:25 -0700

    add waypoint parameter

--- examples/waypoint.py
+++ examples/waypoint.py
@@ -25,6 +25,7 @@ parser_create = subparsers.add_parser('create', help='Create a new waypoint')
 parser_create.add_argument('id', help="id of the waypoint")
 parser_create.add_argument('name', help="name of the waypoint")
 parser_create.add_argument('description', help="description of the waypoint")
+parser_create.add_argument('icon', help="icon of the waypoint")
 parser_create.add_argument('expire', help="expiration date of the waypoint as interpreted by datetime.fromisoformat")
 parser_create.add_argument('latitude', help="latitude of the waypoint")
 parser_create.add_argument('longitude', help="longitude of the waypoint")
@@ -44,6 +45,7 @@ with meshtastic.serial_interface.SerialInterface(args.port, debugOut=d) as iface
             waypoint_id=int(args.id),
             name=args.name,
             description=args.description,
+            icon=args.icon,
             expire=int(datetime.datetime.fromisoformat(args.expire).timestamp()),
             latitude=float(args.latitude),
             longitude=float(args.longitude),
